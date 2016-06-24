149d36e5b6b318f3aa510838f65d16a0f0a70a4e
naka-Eff <jamrodimt@yahoo.co.jp>
2016-06-24 23:39:21 +0900

    stack_Over_Flow

---
+++ src/Q17/Q17.java
@@ -0,0 +1,43 @@
+package Q17;
+
+public class Q17 {
+	private static boolean a = false;
+	private static int count = 0;
+
+	public static void main(String[] args) {
+		execute();
+		System.out.println("最終結果:" + count);
+	}
+
+	private static void execute() {
+		int count = 1;
+		executeImpl(true, count);
+		executeImpl(false, count);
+	}
+
+	private static void executeImpl(boolean boo, int i) {
+		int j = i++;
+		if (j == 31) {
+			check(boo);
+			return;
+		}
+		if (boo) {
+			executeImpl(true, j);
+			executeImpl(false, j);
+		} else {
+			executeImpl(true, j);
+		}
+	}
+
+	/**
+	 * @param boo
+	 *            30人目が男性と女性どちらか 男性なら続きは男性でも女性でもok, 女性なら男性のみ
+	 */
+	private static void check(boolean boo) {
+		if (boo) {
+			count = count + 2;
+		} else {
+			count = count + 1;
+		}
+	}
+}
